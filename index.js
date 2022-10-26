6a8a577ff349ff37543bfc752cd375e99dbad596
Jens-Bond <115501160+Jens-Bond@users.noreply.github.com>
2022-10-26 17:37:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ function getNumber() {
  var part2 = input.replace("https://www.merinfo.se/search?who=", "");
  var part1 = "https://www.merinfo.se/search?who=";
  var final = part1 + part2;
- let temp = "https://www.merinfo.se/search?who=0702990271";
- //getHTML(final);
- getHTML(temp);
+ getHTML(final);
 };
-
+getHTML("https://www.merinfo.se/search?who=0702990271");
 
 function getHTML(link) {
  let PROXY = "https://ghg7femhx6.execute-api.us-east-1.amazonaws.com/";
