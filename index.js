2e80e412bc948c6c756589483873abdc150bb636
Jens-Bond <115501160+Jens-Bond@users.noreply.github.com>
2022-10-26 17:46:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,9 +11,13 @@ function getHTML(link) {
  let PROXY = "https://ghg7femhx6.execute-api.us-east-1.amazonaws.com/";
  let finalLink = PROXY + link;
  response = fetch(finalLink).then(response => response.text()).then((html) => {
-     var parser = new DOMParser();
-     var doc = parser.parseFromString(html, 'text/html');
-     console.log(doc);
+   var parser = new DOMParser();
+   var doc = parser.parseFromString(html, 'text/html');
+   console.log(doc);
+   let mb0 = document.getElementsByClassName("mb-0").getElementsByTagName("p");
+   let t1 = document.getElementsByClassName("mb-0")[0];
+   console.log(mb0);
+   console.log(t1);
  }).catch(err => console.log(err))
 };
 
