107beb147d76134154b3fb5175a47a25b1265347
Jens-Bond <115501160+Jens-Bond@users.noreply.github.com>
2022-10-26 17:31:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ function getNumber() {
 };
 
 
+function getHTML(link) {
+ let PROXY = "https://ghg7femhx6.execute-api.us-east-1.amazonaws.com/";
+ let finalLink = PROXY + link;
+ response = fetch(finalLink).then(response => response.text()).then((html) => {
+     var parser = new DOMParser();
+     var doc = parser.parseFromString(html, 'text/html');
+     console.log(doc);
+ }).catch(err => console.log(err))
+};
+
+
+
 function displayDetails(info) {
    console.log(info);
   
